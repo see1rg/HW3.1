52ee4f2ea8c000d919e42748c170503b164dc95a
Sergey Lyubimov <see1rg@mail.ru>
2022-10-21 22:32:24 +0300

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        PhoneBook.addBook("Ярослав Сахаров",892134012);
+        PhoneBook.addBook("Милана Сорокина",892134013);
+        PhoneBook.addBook("Дарья Журавлева",892134014);
+        PhoneBook.addBook("Ева Логинова",892134015);
+        PhoneBook.addBook("Ярослав Федоров",892134016);
+        PhoneBook.addBook("Аиша Клюева",892134017);
+        PhoneBook.addBook("Милана Елисеева",892134018);
+        PhoneBook.addBook("Алексей Миронов",892134019);
+        PhoneBook.addBook("Виктория Нестерова",892134020);
+        PhoneBook.addBook("Екатерина Тимофеева",892134021);
+        PhoneBook.addBook("Максим Киреев",892134022);
+        PhoneBook.addBook("Вера Волкова",892134023);
+        PhoneBook.addBook("Александр Сухарев",892134024);
+        PhoneBook.addBook("Кристина Горелова",892134025);
+        PhoneBook.addBook("Яков Лебедев",892134026);
+        PhoneBook.addBook("Виктория Орлова",892134027);
+        PhoneBook.addBook("Мария Виноградова",892134028);
+        PhoneBook.addBook("Варвара Черкасова",892134029);
+        PhoneBook.addBook("Роберт Денисов",892134030);
+        PhoneBook.addBook("Алина Черная",892134031);
+
+        System.out.println(PhoneBook.getBook());
+    }
+}
